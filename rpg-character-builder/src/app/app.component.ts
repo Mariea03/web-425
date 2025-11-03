d7db92c88b3c5fb928f664d12d10aae005352be0
Mariea03 <110367002+Mariea03@users.noreply.github.com>
2025-11-02 23:37:55 -0500

    Routing in Action

--- rpg-character-builder/src/app/app.component.ts
+++ rpg-character-builder/src/app/app.component.ts
@@ -11,14 +11,19 @@ import { RouterOutlet, RouterLink } from '@angular/router';
           <img src="RPGCharacter.png" alt="website banner for rpg character builder" class="banner-img">
           </header>
 
+        <div class="sign-in-container">
+          <a routerLink="/signin" class="sign-in-link">Sign In</a>
+        </div>
+
         <main class="main-content">
 
           <nav class="navbar">
            <ul>
              <li><a routerLink="/">Home</a></li>
-             <li><a href="#">Create Character</a></li>
-             <li><a href="#">Your Characters</a></li>
-             <li><a href="#">Log Out</a></li>
+             <li><a routerLink="/players">Players</a></li>
+             <li><a routerLink="/create-character">Create Character</a></li>
+             <li><a routerLink="/create-guild">Create Guild</a></li>
+             <li><a routerLink="/character-faction">Character Faction</a></li>
            </ul>
           </nav>
 
@@ -30,15 +35,31 @@ import { RouterOutlet, RouterLink } from '@angular/router';
         <footer class="footer">
          <nav class="footer-nav">
           <a routerLink="/">Home</a>
-          <a href="#">Create Character</a>
-          <a href="#">Your Characters</a>
-          <a href="#">Log Out</a>
+          <a routerLink="/players">Players</a>
+          <a routerLink="/create-character">Create Character</a>
+          <a routerLink="/create-guild">Create Guild</a>
+          <a routerLink="/create-faction">Create Faction</a>
          </nav>
          <p>&copy;2025 RPG Character Builder</p>
          </footer>
     </div>`,
       styles:[
         `
+        .sign-in-container {
+          text-align:right;
+          padding-right: 20px;
+          margin-top: 10px;
+        }
+
+        .sign-in-link {
+          color: #988731;
+          text-decoration: none;
+          font-family:'Funnel+Sans', sans-serif;
+        }
+
+        .sign-in-link:hover{
+          text-decoration:underline;
+        }
         `
       ]
   })
